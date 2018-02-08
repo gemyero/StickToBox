34df0be60465683ae954059923ae9b8fe2792b09
AsSamha <assamhaa@gmail.com>
2018-02-09 00:16:35 +0200

    added base template file with minor changes

--- books/urls.py
+++ books/urls.py
@@ -5,11 +5,10 @@ from . import views
 urlpatterns=[
 	# re_path(r'^login/', views.login),
 	# re_path(r'^register/', views.register, name='register'),
-	re_path(r'^$',views.home),
-	re_path(r'^$',views.register, name='register'),
+	re_path(r'^$',views.home, name='home'),
 	re_path(r'^login$',views.checkLogin, name='check-login'),
 	re_path(r'^reg$',views.checkRegister, name='check-register'),
-	re_path(r'^home/', views.home, name='home'),
+	# re_path(r'^home/', views.home, name='home'),
 	path('author/<int:pk>', views.AuthorView.as_view(), name='author-detail'),
 	path('authors/', views.AuthorList.as_view(), name='author-list'),
 	path('book/<int:pk>', views.BookView.as_view(), name='book-detail'),
