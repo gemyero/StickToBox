1f6c49a5bb0cd7af9b967b54584884b574b49067
Mohamed Gamal <m.gemyero91@gmail.com>
2018-02-09 23:39:33 +0200

    another changes

--- books/urls.py
+++ books/urls.py
@@ -8,8 +8,6 @@ from django.contrib.auth.views import logout
 app_name = 'books'
 
 urlpatterns=[
-	# re_path(r'^login/', views.login),
-	# re_path(r'^$', list_detail.object_list, all_models_dict, name='home'),
 	re_path(r'^$', views.home, name='home'),
 	re_path(r'^register/', views.register, name='register'),
 	re_path(r'^login$', views.checkLogin, name='check-login'),
@@ -17,13 +15,16 @@ urlpatterns=[
 	re_path(r'^reg$', views.checkRegister, name='check-register'),
 	path('author/<int:pk>', views.AuthorView.as_view(), name='author-detail'),
 	path('authors/', views.AuthorList.as_view(), name='author-list'),
-	path('book/<int:pk>', views.BookView.as_view(), name='book-detail'),
+	path('book/<int:pk>/', views.BookView.as_view(), name='book-detail'),
 	path('books/', views.BookList.as_view(), name='book-list'),
 	path('categories/', views.CategoryList.as_view(), name='category-list'),
     path('categories/<int:pk>', views.CategoryView.as_view(), name='category-detail'),
 	path('service1/<int:id>', views.service1, name="service1"),
+	re_path(r'^book/([0-9]+)/rate$', views.service2, name="service2"),
+    path('service3/', views.service3, name="service3"),
+	re_path(r'^book/([0-9]+)/edit$', views.service4, name="service4"),
 	path('service5/<str:status>', views.service5, name="service5"),
 	path('search/', views.search, name="search"),
 	path('ssearch/<str:keyword>', views.searchService, name="search-service"),
-	#path('service2/<int:id>', views.service2, name="service2"),
+	path('Status/<int:id>', views.Status, name="Status"),
 ]
